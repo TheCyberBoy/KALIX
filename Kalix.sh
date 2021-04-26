bd5de37a87d3618fbdd1fdd9927019031cee9271
Derek <65240836+TheCyberBoy@users.noreply.github.com>
2021-04-26 18:13:23 +0530

    Add files via upload

---
+++ Kalix.sh
@@ -0,0 +1,122 @@
+#kalix by derek (The Cyber Boy)
+
+#colours
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+reset='\033[0m'
+echo " "
+echo -e " $red
+ 
+            ██╗░░██╗░█████╗░██╗░░░░░██╗██╗░░██╗
+            ██║░██╔╝██╔══██╗██║░░░░░██║╚██╗██╔╝
+            █████═╝░███████║██║░░░░░██║░╚███╔╝░
+            ██╔═██╗░██╔══██║██║░░░░░██║░██╔██╗░
+            ██║░╚██╗██║░░██║███████╗██║██╔╝╚██╗
+            ╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚═╝╚═╝░░╚═╝ " |lolcat
+
+echo -e "$yellow                +~+~+~+~+~+~+~+~+~+~+~+~+~ " |lolcat
+echo " "
+echo -e "$green                   <--[Coded by DEREK]-->" |lolcat
+
+echo -e "$cyan                ************************** " |lolcat
+echo -e "$green              <~~(YOUTUBE- THE CYBER BOY)~~>" |lolcat
+echo " "
+echo -e "$yellow                +~+~+~+~+~+~+~+~+~+~+~+~+~ " |lolcat
+sleep 3.50
+echo " "
+echo  $p 'Please Wait ===+['$b'./                   '$p']\' |lolcat
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'..\                  '$p']|' |lolcat
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.../                 '$p']/' |lolcat
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'....\                '$p']\' |lolcat
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'...../               '$p']|' |lolcat
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'......./             '$p']/' |lolcat
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'........\            '$p']|' |lolcat
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'........../          '$p']\' |lolcat
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'..........\          '$p']/' |lolcat
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'............./       '$p']|' |lolcat
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'..............\      '$p']/' |lolcat
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'............./       '$p']\' |lolcat
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'...............\     '$p']|' |lolcat
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'................/    '$p']/' |lolcat
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.................\   '$p']\' |lolcat
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.................../ '$p']|' |lolcat
+sleep 0.4
+clear
+
+echo  "$yellow+++++++++++>$p[Please Wait]$yellow<+++++++++++" |lolcat
+sleep 0.4
+echo " "
+cd $HOME
+
+apt-get update -y
+pkg install wget -y
+pkg install python -y
+pkg install python2 -y
+pkg install openssh -y
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+
+ls
+
+cd kalimux
+
+ls
+
+cp dekal.txt $HOME
+
+ls
+
+cd $HOME
+
+ls
+
+rm -rf kalimux
+
+ls
+
+echo "$p------->$yellow [copy below command] $p<--------" |lolcat
+echo " "
+cat dekal.txt
+echo " "
+echo "$p------->$yellow [installation completed] $p<--------" |lolcat
+echo " "
+sleep 50.10
+echo "$p------->$yellow [Kali Linux is starting wait...] $p<--------" |lolcat
+echo " "
+sleep 4.30
+echo " "
+clear
+rm dekal.txt
+echo " "
+./start-kali.sh
